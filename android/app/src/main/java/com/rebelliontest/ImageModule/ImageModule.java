d30a88bc9e9bb13e0b1b4b514814d469547dbeba
rokkoo <alfonsolasaguirre@gmail.com>
2019-11-22 20:18:10 +0100

    Imageview module added - Android

--- android/app/src/main/java/com/rebelliontest/ImageModule/ImageModule.java
+++ android/app/src/main/java/com/rebelliontest/ImageModule/ImageModule.java
@@ -1,19 +1,12 @@
 package com.rebelliontest.ImageModule;
 
 import android.widget.ImageView;
-import android.widget.SimpleAdapter;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.squareup.picasso.Picasso;
 import androidx.annotation.NonNull;
-
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-
 
 public class ImageModule extends SimpleViewManager<ImageView> {
     private static  final String MODULE_NAME = "ImageModule";
--- android/app/src/main/java/com/rebelliontest/ImageViewPackage.java
+++ android/app/src/main/java/com/rebelliontest/ImageViewPackage.java
@@ -1,28 +1,29 @@
 package com.rebelliontest;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.rebelliontest.ImageModule.ImageModule;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ImageViewPackage implements ReactPackage{
 
+    @NonNull
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
+    @NonNull
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-
-        modules.add(new ImageModule());
-
-        return modules;
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.<ViewManager>singletonList(
+                new ImageModule()
+        );
     }
 }

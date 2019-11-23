f9348e6b26cd3d54432e4eb6d6df9af2dd9dd977
rokkoo <alfonsolasaguirre@gmail.com>
2019-11-23 17:42:26 +0100

    Languages colors

--- theme.js
+++ theme.js
@@ -2,3 +2,12 @@ export const Colors = {
   dark: "#303841",
   light: "#F5F5F5"
 };
+
+export const languagesColors = [
+  {
+    javascript: "#FFD933",
+    vue: "#56DD9F",
+    java: "#BB1616",
+    default: "#8C54A1"
+  }
+];
